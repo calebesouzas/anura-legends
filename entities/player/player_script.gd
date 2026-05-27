099c62bb1cc4a49fa34f73f19fab9dcffc9def42
calebesouzas <cacalebehs@gmail.com>
2026-05-27 16:33:30 -0300

    Put `self` when referencing `Player`

--- entities/player/player_script.gd
+++ entities/player/player_script.gd
@@ -21,51 +21,51 @@ var move_direction: Vector2
 
 func _physics_process(delta: float) -> void:
   # Add the gravity.
-  if not is_on_floor():
-    velocity += get_gravity() * delta
+  if not self.is_on_floor():
+    self.velocity += self.get_gravity() * delta
 
   # Handle jump.
-  if Input.is_action_pressed("jump") and is_on_floor():
-    velocity.y = JUMP_VELOCITY
+  if Input.is_action_pressed("jump") and self.is_on_floor():
+    self.velocity.y = self.JUMP_VELOCITY
 
   # Get the input direction and handle the movement/deceleration.
   # As good practice, you should replace UI actions with custom gameplay actions.
-  input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-  move_direction = get_camera_relative_movement().normalized()
-  if move_direction.length_squared() > 0:
-    velocity.x = move_direction.x * SPEED
-    velocity.z = move_direction.y * SPEED
+  self.input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+  self.move_direction = self.get_camera_relative_movement().normalized()
+  if self.move_direction.length_squared() > 0:
+    self.velocity.x = self.move_direction.x * self.SPEED
+    self.velocity.z = self.move_direction.y * self.SPEED
   else:
-    velocity.x = move_toward(velocity.x, 0, SPEED)
-    velocity.z = move_toward(velocity.z, 0, SPEED)
+    self.velocity.x = move_toward(self.velocity.x, 0, self.SPEED)
+    self.velocity.z = move_toward(self.velocity.z, 0, self.SPEED)
 
-  move_and_slide()
+  self.move_and_slide()
   
   #if Input.is_action_just_pressed("trigger"):
   #  trigger()
 
 
 func get_camera_relative_movement() -> Vector2:
-  var forward: Vector3 = camera.global_transform.basis.z
-  var right: Vector3 = camera.global_transform.basis.x
+  var forward: Vector3 = self.camera.global_transform.basis.z
+  var right: Vector3 = self.camera.global_transform.basis.x
   forward.y = 0
   right.y = 0
   forward = forward.normalized()
   right = right.normalized()
                 
   # PERF: Não acho que seja muito bom ficar alocando tantos 'Vector' intermediários...
-  var direction: Vector3 = forward * input_direction.y + right * input_direction.x
+  var direction: Vector3 = forward * self.input_direction.y + right * self.input_direction.x
   return Vector2(direction.x, direction.z)
 
 func _unhandled_input(event: InputEvent) -> void:
   if event is InputEventScreenDrag:
-    pivot.rotation_degrees.y -= event.screen_relative.x * TOUCH_SENSITIVITY
-    pivot.rotation_degrees.x -= event.screen_relative.y * TOUCH_SENSITIVITY
+    self.pivot.rotation_degrees.y -= event.screen_relative.x * self.TOUCH_SENSITIVITY
+    self.pivot.rotation_degrees.x -= event.screen_relative.y * self.TOUCH_SENSITIVITY
     return
   if event is InputEventMouseMotion:
-    pivot.rotation_degrees.y -= event.screen_relative.x * MOUSE_SENSITIVITY
-    pivot.rotation_degrees.x -= event.screen_relative.y * MOUSE_SENSITIVITY
+    self.pivot.rotation_degrees.y -= event.screen_relative.x * self.MOUSE_SENSITIVITY
+    self.pivot.rotation_degrees.x -= event.screen_relative.y * self.MOUSE_SENSITIVITY
     return
   if event is InputEventKey and event.keycode == KEY_ESCAPE:
-    get_tree().quit()
+    self.get_tree().quit()
     # return is unreachable
