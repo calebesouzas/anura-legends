bd8057fcdd5684a24648f485291ac6b8e47b4ad4
Calebe Souza <cacalebehs@gmail.com>
2026-05-29 07:41:39 -0300

    Make projectiles cause damage

--- entities/projectiles/projectile_script.gd
+++ entities/projectiles/projectile_script.gd
@@ -6,9 +6,6 @@ var owner_id: int
 @export var speed: float
 @export var damage: int
 
-func _init() -> void:
-  self.kind = Kind.BULLET
-
 func setup(shooter_id: int, my_id: int, direction_to_go: Vector3) -> void:
   self.owner_id = shooter_id
   self.id = my_id
@@ -18,4 +15,10 @@ func _physics_process(delta: float) -> void:
   self.velocity = self.direction * self.speed * delta
   if self.move_and_slide():
     var collision: KinematicCollision3D = self.get_last_slide_collision()
+    var collider: Object = collision.get_collider()
+    var entity: AliveEntity = null
+    if collider is AliveEntity:
+      entity = collider as AliveEntity
+    if entity != null:
+      entity.damage(self.damage)
     self.queue_free()
