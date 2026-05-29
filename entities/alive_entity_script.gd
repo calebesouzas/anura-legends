955a352158ba68df99906ab9fd43d0115a01ce36
Calebe Souza <cacalebehs@gmail.com>
2026-05-29 10:49:52 -0300

    Implement respawn system

--- entities/alive_entity_script.gd
+++ entities/alive_entity_script.gd
@@ -2,7 +2,20 @@ class_name AliveEntity extends Entity
 
 var vulnerable: bool = true
 
-var health: int
+@export var initial_health: int = 1000
+
+var health: int = self.initial_health
+
+@export var respawn_delay_seconds: float = 3.0
+@export var respawn_timer: Timer:
+  get:
+    assert(respawn_timer != null, "Should provide respawn Timer node in Inspector")
+    return respawn_timer
+  set(value):
+    respawn_timer = value
+    self.respawn_timer.timeout.connect(self.respawn)
+
+@onready var respawn_global_position: Vector3 = self.global_position
 
 func damage(amount: int) -> void:
   self.branch_damage_effect(amount)
@@ -25,10 +38,20 @@ func play_damage_failure_effect(amount: int) -> void:
 
 func kill() -> void:
   self.play_kill_effect()
-  self.queue_free()
+  self.vulnerable = false # so we don't get killed again without even respawning
+  self.respawn_timer.start(self.respawn_delay_seconds)
 
 func play_kill_effect() -> void:
-  return
+  self.visible = false
+
+func respawn() -> void:
+  self.global_position = self.respawn_global_position
+  self.health = self.initial_health
+  self.vulnerable = true
+  self.play_respawn_effect()
+
+func play_respawn_effect() -> void:
+  self.visible = true
 
 func toggle_vulnerability() -> void:
   self.vulnerable = not self.vulnerable
