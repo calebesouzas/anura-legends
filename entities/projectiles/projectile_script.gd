0ee96d65595143566640322cd907a8765368edff
calebesouzas <cacalebehs@gmail.com>
2026-06-01 17:14:57 -0300

    Introduce bullet gravity

--- entities/projectiles/projectile_script.gd
+++ entities/projectiles/projectile_script.gd
@@ -12,6 +12,8 @@ var owner_id: int
 var reach_squared: float
 var travelled_distance_squared: float
 
+var gravity: float = 0.0
+
 func setup(shooter_id: int, my_id: int, direction_to_go: Vector3) -> void:
   self.owner_id = shooter_id
   self.id = my_id
@@ -19,6 +21,7 @@ func setup(shooter_id: int, my_id: int, direction_to_go: Vector3) -> void:
 
 func _physics_process(delta: float) -> void:
   self.velocity = self.direction * self.speed * delta
+  self.velocity.y += self.gravity
   self.travelled_distance_squared += self.velocity.length_squared()
   if self.move_and_slide():
     var collision: KinematicCollision3D = self.get_last_slide_collision()
@@ -30,4 +33,5 @@ func _physics_process(delta: float) -> void:
       entity.damage(self.damage)
     self.queue_free()
   if self.travelled_distance_squared >= self.reach_squared:
-    self.queue_free()
+    self.speed -= delta * 1000
+    self.gravity -= delta * 9.8
