0e8dd5c0aa6b990159cbc75f8b6c4f76189bb13d
Calebe Souza <cacalebehs@gmail.com>
2026-06-03 08:09:34 -0300

    Use only an index to reference arms in Animator

--- entities/player/player_animator_script.gd
+++ entities/player/player_animator_script.gd
@@ -10,15 +10,7 @@ class_name PlayerAnimator extends AnimationTree
   self.root.get_node("body/arms/left_arm"),
   self.root.get_node("body/arms/right_arm")
 ]
-var other_arm: Node3D
-var other_arm_index: int = 1
-var current_arm: Node3D
-var current_arm_index: int = 0:
-  set(value):
-    current_arm_index = value
-    self.other_arm_index = not current_arm_index
-    self.other_arm = self.arms[self.other_arm_index]
-    self.current_arm = self.arms[current_arm_index]
+var current_arm: int = 0
 
 func play(animation: StringName) -> void:
   self.state_machine.travel(animation)
@@ -37,9 +29,9 @@ func lock_head_at_angle(head_global_rotation: Vector3) -> void:
   self.head.global_rotation = head_global_rotation
 
 func shoot(fire_time: float) -> void:
-  var shoulder_angle_direction: float = -1 if self.current_arm_index == 0 else 1
+  var shoulder_angle_direction: float = -1 if self.current_arm == 0 else 1
   var current_rotation: Vector3 = \
     Vector3.RIGHT * 90 + Vector3(0.0, 0.0, shoulder_angle_direction) * 60
   var tween: Tween = self.create_tween()
-  tween.tween_property(self.arms[self.current_arm_index], "rotation_degrees", current_rotation, fire_time * 0.5)
-  self.current_arm_index = not self.current_arm_index
+  tween.tween_property(self.arms[self.current_arm], "rotation_degrees", current_rotation, fire_time * 0.5)
+  self.current_arm = not self.current_arm
