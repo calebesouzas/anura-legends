f3f8f210d30af89b8199859670de4ceb704fa24f
Henrique Souza <henrique@webdesigncriarte.com.br>
2026-06-13 13:30:28 -0300

    Refactor Player State Machine

--- entities/player/player_script.gd
+++ entities/player/player_script.gd
@@ -3,30 +3,58 @@ class_name Player extends AliveEntity
 func _init() -> void:
   self.health = 1000
 
-enum State {IDLE, RUN, JUMP, FALL, SWIM, DASH}
-enum Flags {
-  MOVE_LOCKED = 1, TRIGGER_LOCKED = 2,
-  GROUNDED = 4,
-  JUMP_PRESSED = 8, SWIM_PRESSED = 16, TRIGGER_PRESSED = 32,
-  JUST_MOVE = 64, JUST_JUMP = 128,
-}
+"""
+State Machine:
+  IDLE_MOVE -> movement, friction (breaking movement)
+    transitions: [JUMP, FALL, SWIM, DASH]
+  JUMP -> jumping with variable height
+    transitions: [IDLE_MOVE, FALL, SWIM, DASH]
+  FALL -> falling with uniform gravity
+    transitions: [IDLE_MOVE, SWIM, ROLL: # special jump #, DASH]
+  ROLL -> [
+    second jump in the air,
+    goes in the direction totally relative to the camera,
+    if `self.dot` is near `-1.0` it will suddenly change direction,
+    otherwise it's gonna just add velocity to the roll direction
+  ]
+    transitions: [IDLE_MOVE, FALL]
+  SWIM -> the Walk In Plasma skill. It gives you a DASH, lowers friction and keeps momentum!
+    transitions: [IDLE_MOVE, FALL, DASH]
+  DASH -> fast impulse towards move direction, with a down force if done in the air or an up
+    force if you jump within a defined tick window!
+    transitions: [IDLE_MOVE, FALL, SWIM]
+"""
+
+enum State {IDLE_MOVE, JUMP, FALL, ROLL, SWIM, DASH}
+var move_locked: bool
+var swim_locked: bool
+var trigger_locked: bool
+var jump_pressed: bool
+var swim_pressed: bool
+var trigger_pressed: bool
+var just_move: bool
+var just_jump: bool
+var just_dash: bool
+var grounded: bool
+var wanna_move: bool
 
 var state: State
 signal state_changed(old_state: State, new_state: State)
-var flags: int = 0
+var state_ticks: int = 0
 
 var aim_locked: bool = false
 
 @export_group("Physics")
-@export var IDLE_SPEED: float = 5.0
-@export var RUN_SPEED: float = 7.0
+@export var IDLE_MOVE_SPEED: float = 5.0
 @export var FALL_SPEED: float = 5.0
 @export var JUMP_SPEED: float = 8.0
+@export var ROLL_SPEED: float = JUMP_SPEED
 @export var GROUND_ACCELERATION: float = 21.0
 @export var AIR_ACCELERATION: float = 10.5
 @export var JUMP_FORCE: float = 5.0
 @export var JUMP_CURVE: Curve
 @export var GRAVITY: float = 12.0
+@export var FRICTION: float = 10.0
 @export var MOVE_DEADZONE: float = 0.2
 @export var ADD_POINT: float = 8.0
 var time: float = 0.0
@@ -89,7 +117,7 @@ var airbone_time: float = 0
 @export var shots_per_second: float:
   set(value):
     shots_per_second = value
-    self.fire_time = 1.0 / shots_per_second
+    fire_time = 1.0 / shots_per_second
 var fire_time: float
 
 var input_direction: Vector2
@@ -103,222 +131,204 @@ func _ready() -> void:
   if not OS.has_feature("android"):
     %hud.queue_free()
   Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-  self.aim_lock_timer.timeout.connect(func(): self.aim_locked = false)
-  self.skin.visibility_changed.connect(func(): self.health_indicator.visible = not self.skin.visible)
-  self.health_indicator.visible = false
+  aim_lock_timer.timeout.connect(func(): aim_locked = false)
+  skin.visibility_changed.connect(func(): health_indicator.visible = not skin.visible)
+  health_indicator.visible = false
   for state_key: StringName in State.keys():
-    self.state_speeds.append(self[state_key + "_SPEED"])
+    state_speeds.append(self[state_key + "_SPEED"])
   # self.state_changed.connect(func(_old: State, new: State): print(State.keys()[new]))
 
 func _physics_process(delta: float) -> void:
-  self.time += delta
-  self.camera_direction = self.camera.global_position.direction_to(
-    self.aim.global_position
+  time += delta
+  state_ticks += 1
+  camera_direction = camera.global_position.direction_to(
+    aim.global_position
   )
 
-  self.ground_speed = Vector2(self.velocity.x, self.velocity.z).length()
+  ground_speed = Vector2(velocity.x, velocity.z).length()
 
-  if self.is_on_floor():
-    self.flags |= Flags.GROUNDED
-    if self.airbone_time > 0: print("Air time ", self.airbone_time)
-    self.airbone_time = 0
+  grounded = is_on_floor()
+  if grounded:
+    if airbone_time > 0: print("Air time ", airbone_time)
+    airbone_time = 0
   else:
-    self.flags &= ~Flags.GROUNDED
-    self.airbone_time += delta
+    airbone_time += delta
 
-  if not self.flags & Flags.MOVE_LOCKED:
-    self.input_direction = self.get_movement()
-    self.prev_move_len = self.move_direction.length_squared()
-    self.move_direction = Vector3(self.input_direction.x, 0.0, self.input_direction.y).rotated(Vector3.UP, self.pivot.rotation.y)
-    self.curr_move_len = self.move_direction.length_squared()
-    if is_equal_approx(self.prev_move_len, 0) and self.curr_move_len > self.MOVE_DEADZONE:
-      self.flags |= Flags.JUST_MOVE
-    else:
-      self.flags &= ~Flags.JUST_MOVE
+  if not move_locked:
+    input_direction = get_movement()
+    prev_move_len = move_direction.length_squared()
+    move_direction = Vector3(input_direction.x, 0.0, input_direction.y).rotated(Vector3.UP, pivot.rotation.y)
+    curr_move_len = move_direction.length_squared()
+    wanna_move = curr_move_len > MOVE_DEADZONE ** 2
+    just_move = is_equal_approx(prev_move_len, 0) and wanna_move
 
-  self.dot = self.velocity.dot(self.move_direction)
+  dot = velocity.dot(move_direction)
 
-  if Input.is_action_just_pressed("jump"):
-    self.flags |= Flags.JUST_JUMP
-  elif Input.is_action_pressed("jump"):
-    self.flags &= ~Flags.JUST_JUMP
-    self.flags |= Flags.JUMP_PRESSED
-  else:
-    self.flags &= ~Flags.JUMP_PRESSED
-    self.flags &= ~Flags.JUST_JUMP
+  just_jump = Input.is_action_just_pressed("jump")
+  jump_pressed = Input.is_action_pressed("jump")
 
-  if Input.is_action_pressed("swim"):
-    self.flags |= Flags.SWIM_PRESSED
-  else:
-    self.flags &= ~Flags.SWIM_PRESSED
+  swim_pressed = Input.is_action_pressed("swim")
 
-  if not self.flags & Flags.TRIGGER_LOCKED:
-    if Input.is_action_pressed("trigger"):
-      self.flags |= Flags.TRIGGER_PRESSED
-    else:
-      self.flags &= ~Flags.TRIGGER_PRESSED
+  trigger_pressed = not trigger_locked and Input.is_action_pressed("trigger")
 
-  self.speed = self.state_speeds[self.state]
-  self.final_speed = lerp(
+  just_dash = Input.is_action_just_pressed("dash")
+
+  speed = state_speeds[state]
+  final_speed = lerp(
     speed,
-    speed + (self.ground_speed if self.ground_speed > self.ADD_POINT else 0.0),
+    speed + (ground_speed if ground_speed > ADD_POINT else 0.0),
     delta
   )
-  self.handle_state(delta)
+  handle_state(delta)
 
-  self.move_and_slide()
+  move_and_slide()
 
-  self.health_indicator.text = str(self.health)
+  health_indicator.text = str(health)
 
-  if self.flags & Flags.TRIGGER_PRESSED and not self.flags & Flags.TRIGGER_LOCKED \
-      and self.fire_locked_timer.is_stopped():
-    self.aim_locked = true
-    self.aim_lock_timer.start()
-    self.fire_locked_timer.start(self.fire_time)
-    self.trigger()
+  if trigger_pressed and fire_locked_timer.is_stopped():
+    aim_locked = true
+    aim_lock_timer.start()
+    fire_locked_timer.start(fire_time)
+    trigger()
 
-  if self.move_direction.length_squared() <= pow(self.MOVE_DEADZONE, 2):
+  if not wanna_move:
     return
 
   var target_angle: float = \
-    Vector3.FORWARD.signed_angle_to(self.camera_direction, Vector3.UP) \
-    if self.aim_locked else Vector3.FORWARD.signed_angle_to(self.velocity, Vector3.UP)
-  self.skin.rotation.y = lerp_angle(
-    self.skin.rotation.y,
+    Vector3.FORWARD.signed_angle_to(camera_direction, Vector3.UP) \
+      if aim_locked else Vector3.FORWARD.signed_angle_to(velocity, Vector3.UP)
+  skin.rotation.y = lerp_angle(
+    skin.rotation.y,
     target_angle,
-    self.ROTATION_SPEED * 2.0 * delta
+    ROTATION_SPEED * 2.0 * delta
   )
 func handle_state(delta: float) -> void:
-  match self.state:
-    State.IDLE:
-      self.velocity.x = move_toward(self.velocity.x, 0, self.GROUND_ACCELERATION * delta)
-      self.velocity.z = move_toward(self.velocity.z, 0, self.GROUND_ACCELERATION * delta)
+  match state:
+    State.IDLE_MOVE:
+      if wanna_move:
+        move_2d(move_direction, GROUND_ACCELERATION, delta)
+      else:
+        move_2d(Vector3.ZERO, FRICTION, delta)
 
-      if self.input_direction.length_squared() > self.MOVE_DEADZONE*self.MOVE_DEADZONE:
-        self.set_state(State.RUN) #@issue one tick of input lag for movement...
-      elif not self.flags & Flags.GROUNDED:
-        self.set_state(State.FALL)
-      elif self.flags & Flags.JUMP_PRESSED:
-        self.set_state(State.JUMP)
-      elif self.flags & Flags.SWIM_PRESSED:
-        self.set_state(State.SWIM)
-
-    State.RUN:
-      self.move_2d(self.move_direction, self.GROUND_ACCELERATION, delta)
-
-      if self.input_direction.length_squared() < self.MOVE_DEADZONE*self.MOVE_DEADZONE:
-        self.set_state(State.IDLE)
-      elif not self.flags & Flags.GROUNDED:
-        self.set_state(State.FALL)
-      elif self.flags & Flags.JUMP_PRESSED:
-        self.set_state(State.JUMP)
-      elif self.flags & Flags.SWIM_PRESSED:
-        self.set_state(State.SWIM)
+      if not grounded:
+        set_state(State.FALL)
+      elif jump_pressed:
+        set_state(State.JUMP)
+      elif swim_pressed:
+        set_state(State.SWIM)
+      elif just_dash:
+        set_state(State.DASH)
 
     State.FALL:
-      if self.flags & Flags.GROUNDED:
-        if self.flags & Flags.SWIM_PRESSED:
-          self.set_state(State.SWIM)
-        else:
-          self.set_state(State.IDLE)
+      if grounded:
+        set_state(State.SWIM if swim_pressed else State.IDLE_MOVE)
+        return
+      elif just_dash:
+        set_state(State.DASH)
         return
 
-      self.move_2d(self.move_direction, self.AIR_ACCELERATION, delta)
-      self.velocity.y -= self.GRAVITY * delta
+      move_2d(move_direction, AIR_ACCELERATION, delta)
+      velocity.y -= GRAVITY * delta
 
     State.JUMP:
-      if not self.flags & Flags.JUMP_PRESSED or self.jump_force <= 0:
-        self.jump_force = self.JUMP_FORCE
-        self.set_state(State.FALL)
+      if not jump_pressed or jump_force <= 0:
+        jump_force = JUMP_FORCE
+        set_state(State.FALL)
+        return
+      elif just_dash:
+        set_state(State.DASH)
         return
 
-      self.move_2d(self.move_direction, self.AIR_ACCELERATION, delta)
-      self.velocity.y = self.jump_force
-      self.jump_force = self.JUMP_CURVE.sample(self.airbone_time) * self.JUMP_FORCE
+      move_2d(move_direction, AIR_ACCELERATION, delta)
+      velocity.y = jump_force
+      jump_force = JUMP_CURVE.sample(airbone_time) * JUMP_FORCE
 
     State.DASH:
-      self.dash_ticks += 1
-      if self.dash_ticks > self.DASH_DURATION_IN_TICKS:
-        self.set_state(State.SWIM)
-        self.dash_ticks = 0
-        self.flags &= ~Flags.MOVE_LOCKED
-        self.flags &= ~Flags.TRIGGER_LOCKED
+      if state_ticks > DASH_DURATION_IN_TICKS:
+        set_state(State.FALL)
+        move_locked = false
+        trigger_locked = false
         return
 
-      if self.dash_ticks == 1:
-        self.flags |= Flags.MOVE_LOCKED
-        self.flags |= Flags.TRIGGER_LOCKED
+      if state_ticks == 1:
+        move_locked = true
+        trigger_locked = true
         #@todo effect!
 
-      if self.flags & Flags.GROUNDED:
-        self.move_2d(self.move_direction, self.DASH_ACCELERATION, delta)
+      if grounded:
+        move_2d(move_direction, DASH_ACCELERATION, delta)
       else:
-        self.move_3d(self.move_direction + Vector3.DOWN, self.DASH_ACCELERATION, delta)
+        move_3d(move_direction + Vector3.DOWN, DASH_ACCELERATION, delta)
 
     State.SWIM:
-      if not self.plasma_manager.can_swim(self.feet_ray.get_collision_point(), self.team_color) \
-          or not self.flags & Flags.SWIM_PRESSED:
-        self.flags &= ~Flags.TRIGGER_LOCKED
-        self.skin.visible = true
-        self.set_state(State.RUN)
+      if not plasma_manager.can_swim(feet_ray.get_collision_point(), team_color) \
+          or not swim_pressed:
+        trigger_locked = false
+        skin.visible = true
+        set_state(State.IDLE_MOVE)
         return
 
-      self.flags |= Flags.TRIGGER_LOCKED
+      trigger_locked = true
       #@todo effect!
-      self.skin.visible = false
-      self.move_2d(
-        self.move_direction,
-        self.SWIM_OPPOSITE_ACCELERATION if self.dot < 0.0 else self.SWIM_ACCELERATION,
+      skin.visible = false
+      move_2d(
+        move_direction,
+        SWIM_OPPOSITE_ACCELERATION if dot < 0.0 else SWIM_ACCELERATION,
         delta
       )
 
-      if self.flags & Flags.JUST_MOVE and self.flags & Flags.JUMP_PRESSED:
-        self.set_state(State.DASH)
+      if just_dash:
+        set_state(State.DASH)
+      elif jump_pressed:
+        set_state(State.JUMP)
+      elif not grounded:
+        set_state(State.FALL)
     _:
-      assert(false, "Unhandled state: " + State.keys()[self.state])
+      assert(false, "Unhandled state: " + State.keys()[state])
 
 func set_state(new_state: State) -> void:
-  self.state_changed.emit(self.state, new_state)
-  self.state = new_state
+  state_changed.emit(state, new_state)
+  state = new_state
+  state_ticks = 0
 
 func move_3d(direction: Vector3, acceleration: float, delta: float) -> void:
-  self.velocity = self.velocity.move_toward(direction * self.final_speed, acceleration * delta)
+  velocity = velocity.move_toward(direction * final_speed, acceleration * delta)
 
 func move_2d(direction: Vector3, acceleration: float, delta: float) -> void:
-  self.velocity.x = move_toward(self.velocity.x, direction.x * self.final_speed, acceleration * delta)
-  self.velocity.z = move_toward(self.velocity.z, direction.z * self.final_speed, acceleration * delta)
+  velocity.x = move_toward(velocity.x, direction.x * final_speed, acceleration * delta)
+  velocity.z = move_toward(velocity.z, direction.z * final_speed, acceleration * delta)
 
 func trigger() -> void:
-  self.plasma_manager.spawn_new_projectile(self.id, self.bullet_scene, self.team_color, self.camera_direction)
+  plasma_manager.spawn_new_projectile(id, bullet_scene, team_color, camera_direction)
 
 func get_movement() -> Vector2:
   return Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 
 func has_the_dot() -> bool:
-  return self.DOT_POINT - self.DOT_RANGE < self.dot and self.dot < self.DOT_POINT + self.DOT_RANGE
+  return DOT_POINT - DOT_RANGE < dot and dot < DOT_POINT + DOT_RANGE
 
 func _unhandled_input(event: InputEvent) -> void:
   var is_camera_motion: bool = false
   var camera_motion: Vector2 = Vector2.ZERO
   if event is InputEventScreenDrag:
-    camera_motion = event.screen_relative * self.TOUCH_SENSITIVITY
+    camera_motion = event.screen_relative * TOUCH_SENSITIVITY
     is_camera_motion = true
   elif event is InputEventMouseMotion:
-    camera_motion = event.screen_relative * self.MOUSE_SENSITIVITY
+    camera_motion = event.screen_relative * MOUSE_SENSITIVITY
     is_camera_motion = true
 
   if is_camera_motion:
-    self.pivot.rotation_degrees.y -= camera_motion.x
-    self.pivot.rotation_degrees.x -= camera_motion.y
+    pivot.rotation_degrees.y -= camera_motion.x
+    pivot.rotation_degrees.x -= camera_motion.y
     # don't accumulate rotation indefinitelly
-    self.pivot.rotation_degrees.y = wrapf(
-      self.pivot.rotation_degrees.y, 0.0, 360.0
+    pivot.rotation_degrees.y = wrapf(
+      pivot.rotation_degrees.y, 0.0, 360.0
     )
     # limit vertical rotation to prevent camera getting upside-down and breaking
-    self.pivot.rotation_degrees.x = clampf(
-      self.pivot.rotation_degrees.x,
+    pivot.rotation_degrees.x = clampf(
+      pivot.rotation_degrees.x,
       -85.0, # look down limit
       60.0 # look up limit
     )
   elif event is InputEventKey and event.keycode == KEY_ESCAPE:
-    self.get_tree().quit()
+    get_tree().quit()
