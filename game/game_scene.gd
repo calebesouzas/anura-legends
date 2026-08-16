1da3bf5c3cfa5b22cd3f76a11761161fc6efa6a8
calebesouzas <cacalebehs@gmail.com>
2026-08-16 12:35:37 -0300

    Add controller (dis)connection signal handlers

--- game/game_scene.gd
+++ game/game_scene.gd
@@ -12,3 +12,11 @@ func _ready() -> void:
   const path: StringName = "res://levels/practice/practice_scene.tscn"
   var world: World = World.new(path, self.plasma_manager)
   self.plasma_manager.setup(world)
+  Input.joy_connection_changed.connect(_on_joy_connection_changed)
+
+func _on_joy_connection_changed(device_id: int, connected: bool) -> void:
+  if connected:
+    print("Controller connected at index: ", device_id)
+    print("Controller name: ", Input.get_joy_name(device_id))
+  else:
+    print("Controller disconnected from index: ", device_id)
