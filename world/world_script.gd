a3ec58982047b7df2e2cd98a55070fb222e4bc27
calebesouzas <cacalebehs@gmail.com>
2026-08-23 18:02:20 -0300

    Remove `self` explicit references at world script

--- world/world_script.gd
+++ world/world_script.gd
@@ -7,13 +7,15 @@ var scene: Node3D
 # Had to use `new_world` name because `new` is like a keyword and also a reserved static function
 func _init(scene_path: StringName, plasma_manager_reference: PlasmaManager) -> void:
   assert(plasma_manager_reference != null, "Should provide a non-null plasma manager reference")
-  self.plasma_manager = plasma_manager_reference
+  plasma_manager = plasma_manager_reference
 
   assert(scene_path.ends_with(".tscn"), "Should provide a scene file to World")
-  self.scene = (load(scene_path) as PackedScene).instantiate()
-  self.add_child(self.scene)
+  scene = (load(scene_path) as PackedScene).instantiate()
+  add_child(scene)
 
-func _ready() -> void:  
-  var spawn_point_node: Marker3D = self.scene.get_node("spawn_point")
+var astars: Array[AStar3D]
+
+func _ready() -> void:
+  var spawn_point_node: Marker3D = scene.get_node("spawn_point")
   if spawn_point_node != null:
-    self.spawn_point_global_position = spawn_point_node.global_position
+    spawn_point_global_position = spawn_point_node.global_position
